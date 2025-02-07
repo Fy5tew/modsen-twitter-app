fbba8111ae4ed873db0119e8e16478414d03f1b4
Fy5tew <nikturchinovich@mail.ru>
2025-02-07 03:01:32 +0300

    feat: add validation utils

---
+++ src/utils/formShema.ts
@@ -0,0 +1,29 @@
+import * as yup from 'yup';
+import { InferType } from 'yup';
+
+import {
+    birthDate,
+    email,
+    login,
+    name,
+    password,
+    passwordConfirmation,
+    phone,
+} from './formValidators';
+
+export type ILoginForm = InferType<typeof loginForm>;
+export type IRegisterForm = InferType<typeof registerForm>;
+
+export const loginForm = yup.object({
+    login: login,
+    password: password,
+});
+
+export const registerForm = yup.object({
+    name: name,
+    phone: phone,
+    email: email,
+    password: password,
+    passwordConfirmation: passwordConfirmation(),
+    dateOfBirth: birthDate,
+});
